a695f468f7dd8565e58400b390a0e7530a4bc20a
Soundarya-2002 <139782825+Soundarya-2002@users.noreply.github.com>
2023-08-09 19:01:55 +0530

    Add files via upload
    BankInfo

---
+++ BankMethodOverriding/AxisBank.java
@@ -0,0 +1,14 @@
+package BankMethodOverriding;
+
+public class AxisBank extends BankInfo {
+      public void Deposit() {
+    	  System.out.println("AxisBank Deposit");
+      }
+  public static void main(String[]args) {
+	  AxisBank Bank=new AxisBank();
+	  Bank.Deposit();
+	  Bank.Saving();
+	  Bank.Fixed();
+  }
+}
+  
---
+++ BankMethodOverriding/BankInfo.java
@@ -0,0 +1,13 @@
+package BankMethodOverriding;
+
+public class BankInfo {
+	public void Saving() {
+		System.out.println("Saving");
+	}
+	public void Fixed() {
+		System.out.println("Fixed");
+	}
+	public void Deposit() {
+		System.out.println("Deposit");
+	}
+}
